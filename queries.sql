db7f4ce6015b6c5f3bd3e1c2d64bdaa8599f2757
benshidanny11 <benshidanny11@gmail.com>
2022-07-06 08:44:35 +0200

    Impement animal table and data manipulation

--- queries.sql
+++ queries.sql
@@ -1,3 +1,16 @@
 /*Queries that provide answers to the questions from all projects.*/
+SELECT * FROM animals WHERE name LIKE '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+
+SELECT * FROM animals WHERE date_of_birth between '2016-01-01' ADD '2019-12-31';
+
+SELECT * FROM animals WHERE neutered=true AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
+
+ SELECT * FROM animals WHERE neutered=true;
+
+
+SELECT * FROM animals WHERE name <> 'Gabumon';
+
+
--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+\c vet_clinic;
+
+CREATE TABLE animals(id serial primary key,name text,date_of_birth date,escape_attempts integer,neutered boolean,weight_kg float);
