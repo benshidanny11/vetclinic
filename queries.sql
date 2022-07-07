a4e272b6efacd7798e7aa3b98ffcdf37f203fa42
benshidanny11 <benshidanny11@gmail.com>
2022-07-07 11:22:02 +0200

    Implement queries and update animals

--- queries.sql
+++ queries.sql
@@ -13,4 +13,45 @@ SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
 
 SELECT * FROM animals WHERE name <> 'Gabumon';
 
+BEGIN; 
+update animals set species='unspecified';
+
+ROLLBACK;
+
+UPDATE animals SET species='digimon' WHERE name like '%mon';
+
+UPDATE animals SET species='pokemon' WHERE species='';
+
+COMMIT;
+
+BEGIN; DELETE from animals;
+
+ROLLBACK;
+
+ BEGIN; DELETE from animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT sp_1;
+
+UPDATE animals SET weight_kg=weight_kg*-1;
+
+ROLLBACK TO sp_1;
+
+
+ UPDATE animals SET weight_kg=weight_kg*-1 where weight_kg<0;
+
+ SELECT count(*) FROM animals;
+
+SELECT count(*) FROM animals WHERE escape_attempts=0;
+
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts) FROM animals WHERE neutered=true OR neutered=false;
+SELECT MAX(weight_kg),MIN(weight_kg) FROM animals WHERE species='digimon';
+
+
+
+
+
+
+
+
 
--- schema.sql
+++ schema.sql
@@ -4,3 +4,5 @@ CREATE DATABASE vet_clinic;
 \c vet_clinic;
 
 CREATE TABLE animals(id serial primary key,name text,date_of_birth date,escape_attempts integer,neutered boolean,weight_kg float);
+
+ALTER TABLE animals ADD COLUMN species text;
