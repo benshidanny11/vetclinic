c30e0414bb4de17144392003c000250944389359
benshidanny11 <benshidanny11@gmail.com>
2022-07-09 09:45:31 +0200

    Implement join visits

--- queries.sql
+++ queries.sql
@@ -57,12 +57,23 @@ select * from animals where owner_id=2 and species_id=2;
 
  select full_name,age,owner_id from owners LEFT JOIN animals ON owners.id=animals.owner_id group by full_name,age,owner_id order by count(*) desc limit 1;
 
+SELECT animals.name,animals.date_of_birth,animals.weight_kg, max(visits.date_of_visit) AS visit_date from visits left join animals on animals.id=visits.animals_id where vets_id=1 group by animals.name,animals.date_of_birth,animals.weight_kg limit 1;
 
+select count(*) from animals join visits on animals.id=visits.animals_id where visits.vets_id=2; 
 
+select * from vets left join specializations on vets.id=specializations.vets_id left join species on species.id=specializations.species_id;
 
+SELECT animals.* from animals inner join visits on animals.id=visits.animals_id where visits.date_of_visit between '2020-04-01' and '2020-08-30' and visits.vets_id=3;
 
+ SELECT animals.name,animals.date_of_birth,animals.weight_kg,count(*) as visit_count from animals INNER JOIN visits ON animals.id=visits.animals_id group by animals.name,animals.date_of_birth,animals.weight_kg order by count(*) desc limit 1;
 
+SELECT animals.name,animals.date_of_birth,animals.weight_kg, max(visits.date_of_visit) as visit_date from visits left join animals on animals.id=visits.animals_id where vets_id=4 group by animals.name,animals.date_of_birth,animals.weight_kg limit 1;
 
+SELECT animals.name as animal_name,animals.date_of_birth as animal_dob,animals.weight_kg as animal_weight,vets.name as vetname,vets.age as vet_age,vets.date_of_graduation as vet_graduation_date, min(visits.date_of_visit) as visit_date from visits inner join animals on animals.id=visits.animals_id inner join vets on visits.vets_id=vets.id group by animals.name,animals.date_of_birth,animals.weight_kg,vets.name,vets.age,vets.date_of_graduation order by  max(visits.date_of_visit) asc limit 1;
+
+SELECT count(*) from visits inner join vets on visits.vets_id=vets.id left join specializations on specializations.vets_id=vets.id where specializations.vets_id is null;
+
+SELECT species.name from animals inner join visits on animals.id=visits.animals_id inner join vets on vets.id=visits.vets_id inner join species on animals.species_id=species.id where vets.id=2 group by species.name order by count(*) desc limit 1;
 
 
 
--- schema.sql
+++ schema.sql
@@ -17,3 +17,13 @@ ALTER TABLE animals ADD COLUMN owner_id integer;
 ALTER TABLE animals ADD CONSTRAINT fk_animal_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
 
 ALTER TABLE animals ADD CONSTRAINT fk_animal_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+CREATE TABLE vets(id serial PRIMARY KEY,name TEXT, age INTEGER, date_of_graduation DATE);
+
+CREATE TABLE specializations(species_id INTEGER, vets_id INTEGER, CONSTRAINT FK_species FOREIGN KEY (species_id) REFERENCES species (id), CONSTRAINT FK_vets FOREIGN KEY (vets_id) REFERENCES vets (id) );
+
+CREATE TABLE visits(animals_id INTEGER, vets_id INTEGER, CONSTRAINT FK_animals FOREIGN KEY (animals_id) REFERENCES animals (id), CONSTRAINT FK_vets FOREIGN KEY (vets_id) REFERENCES vets (id) );
+
+ALTER TABLE visits ADD COLUMN date_of_visit DATE;
+
+
